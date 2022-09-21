25a23bdf38ba37b2d7a71c8452cb79bfb56fae86
saif <saifalis@axioned.com>
2022-09-21 14:13:09 +0530

    js code added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,42 @@
-/* Author: 
+// progress bar function
+var progress = document.querySelectorAll('.progress-list');
+var prevBtn = document.querySelector('#prev-btn');
+var nextBtn = document.querySelector('#next-btn');
 
-*/
+counter = 1;
+nextBtn.addEventListener('click', function (){
+  counter++;
+  
+  if (counter <= progress.length) {
+    update();
+  }
+})
+prevBtn.addEventListener('click', function (){
+  counter--;
+  
+  if (counter <= progress.length) {
+    update();
+  }
+})
+
+function update() {
+  progress.forEach(function (li , index) {
+    if (index < counter) {
+      li.classList.add('progress-list-active');
+    } else {
+      li.classList.remove('progress-list-active');
+    }
+  });
+
+  if (counter === 1) {
+    prevBtn.setAttribute('disabled',true);
+  } else if (counter === progress.length) {
+    nextBtn.setAttribute('disabled',true);
+  } else {
+    prevBtn.removeAttribute('disabled');
+    nextBtn.removeAttribute('disabled');
+  }
+}
 
 
 
